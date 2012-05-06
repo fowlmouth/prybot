b6080baf05613444decf9de83a6cf3259ba18db9
Tim Eichholz <phowl.mouth@gmail.com>
2012-05-06 03:55:54 -0400

    translate plugin

---
+++ plugins/translate.cinch2.rb
@@ -0,0 +1,13 @@
+require'google_translate'
+
+module Prugins
+class GoogleTranslate
+  include Cinch::Plugin
+  set :prefix, PREFIX
+  match /translate (\S+) (\S+) (.+)/, method: :trans
+
+  def trans(m, from, to, text)
+    m.reply ::Google::Translator.new.translate(from, to, text).join(' // ')
+  end
+end
+end
